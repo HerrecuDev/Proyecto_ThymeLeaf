4a08ac368ebc4dcca6aa8e2b173beae516ec0425
HerrecuDev <phercue2805>
2025-11-11 21:23:55 +0100

    Hola nuevo cambio

--- src/main/java/org/iesdm/thymeleaf_practica/controller/DespedidaController.java
+++ src/main/java/org/iesdm/thymeleaf_practica/controller/DespedidaController.java
@@ -1,5 +1,9 @@
 package org.iesdm.thymeleaf_practica.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DespedidaController {
 
 
