59651ce076ec829d78e4b50884acbbcdcde4b11b
HerrecuDev <phercue2805@g.educaand.es>
2025-11-12 12:04:13 +0100

    cambios

--- src/main/java/org/iesdm/thymeleaf_practica/controller/SaludosController.java
+++ src/main/java/org/iesdm/thymeleaf_practica/controller/SaludosController.java
@@ -3,6 +3,7 @@ package org.iesdm.thymeleaf_practica.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SaludosController {
@@ -18,5 +19,9 @@ public class SaludosController {
 
         return "saludo";
     }
+
+    @PostMapping("/saludos")
+
+    public String
 
 }
--- src/main/java/org/iesdm/thymeleaf_practica/dto/SaludoDTO.java
+++ src/main/java/org/iesdm/thymeleaf_practica/dto/SaludoDTO.java
@@ -1,4 +1,21 @@
 package org.iesdm.thymeleaf_practica.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SaludoDTO {
+
+    //Dto Data transfer Object
+
+        private String nombre;
+        private String idioma;
+
+
+
 }
