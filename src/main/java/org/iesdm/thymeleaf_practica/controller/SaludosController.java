c91ebd6fe112ac8654bbf6a80b4dac84dd43f3a7
HerrecuDev <phercue2805@g.educaand.es>
2025-11-12 12:04:21 +0100

    mejoprar

--- src/main/java/org/iesdm/thymeleaf_practica/controller/SaludosController.java
+++ src/main/java/org/iesdm/thymeleaf_practica/controller/SaludosController.java
@@ -1,27 +1,35 @@
 package org.iesdm.thymeleaf_practica.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.iesdm.thymeleaf_practica.dto.SaludoDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+@Slf4j
 @Controller
 public class SaludosController {
 
     //Generamos el Empoint para la ruta http://local:8080/saludo
 
-    @GetMapping("/saludo")
-    public String saludar(Model model){
+    @GetMapping("/saludos")
+    public String saludar(Model model ,@ModelAttribute SaludoDTO saludoDTO){
 
 
 
 
 
-        return "saludo";
+        return "saludos";
     }
 
     @PostMapping("/saludos")
 
-    public String
+    public String saludosSubmit(Model model,@ModelAttribute SaludoDTO saludoDTO){
+
+        log.info(saludoDTO.toString());
+
+        return "saludos";
+    }
 
 }
